d39938d552bd54a316a2481df72607338b29ecc8
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 09:55:25 +0900

    yy

--- test.pym.py
+++ test.pym.py
@@ -3,7 +3,7 @@ from pyplus.perceptron.syntax_implement import reflect
 from pyplus.perceptron.graph import Node,default_optimizer
 
 model LinearRegression:
-    hidden_1st := X*Node(W1,is_parameter=True)
+    hidden_1st := X*?W1
     hidden_2nd := hidden_1st*Node(Dense(4),is_parameter=True)
     out = hidden_2nd*Node(Dense(1),is_parameter=True)
     Y ?= out
@@ -14,10 +14,10 @@ Node(xa.vi.er.viv.ss.bob.we.void())
 
 
 model LinearRegression:
-    Y ?= a*Node(1,is_parameter=True)
+    Y ?= a*?1
 
 model Arithmetic:
-    3 ?= Node(4,is_parameter=True)-1
+    3 ?= ?4-1
 
 
 model TorchExample:
@@ -26,4 +26,4 @@ model TorchExample:
     if __cur_epo__ % 100 == 99:
         print(__cur_epo__,__cur__err__)
     
-    y ?= Node(0,is_parameter=True) + Node(0,is_parameter=True)*x + Node(0,is_parameter=True)*x**2 + Node(0,is_parameter=True)*x**3
+    y ?= ?0 + ?0.*x + ?0*x**2 + ?0*x**3
