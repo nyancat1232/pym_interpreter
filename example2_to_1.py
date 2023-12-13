f172023e33fcf548116cfd01943943ff86abd7a6
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 21:21:40 +0900

    wow

--- example2_to_1.py
+++ example2_to_1.py
@@ -6,26 +6,24 @@
 import torch
 from pyplus.pytorch.simple import TorchPlus
 
+import torch
+from pyplus.pytorch.simple import TorchPlus
+
 tp = TorchPlus()
 tp.meta_optimizer = torch.optim.SGD
-tp.meta_optimizer_epoch = 20
-tp.meta_optimizer_learning_rate = 0.05
+tp.meta_optimizer_epoch = 300
+tp.meta_optimizer_learning_rate = 0.01
 tp.meta_error_measurement = torch.nn.MSELoss
-def SimpleExample(_tp:TorchPlus):
-    #all terminals
-    _tp.all_tensors['___con_1'] = torch.FloatTensor([2.])
-    _tp.all_tensors['___con_2'] = torch.FloatTensor([10.])
-    _tp.all_tensors['___par_1'] = torch.rand(_tp.all_tensors['___con_1'].shape[-1], requires_grad=True)
-    
-    for _ in range(_tp.meta_optimizer_epoch):
-        #process
-        pred = _tp.all_tensors['___par_1'] * _tp.all_tensors['___con_1']
-        label = _tp.all_tensors['___con_2']
-
-        #train
-        _tp.train_one_step_by_equation(label,pred)
-    return _tp.get_all_params()
+def assign_values(self):
+    self.all_tensors['___con_1'] = torch.FloatTensor([10])
+    self.all_tensors['___con_2'] = torch.FloatTensor([2])
+    self.all_tensors['___par_1'] = torch.rand(1, requires_grad=True)
+tp.assign_process_values=assign_values
+def assign_process(self):
+    self._pred = self.all_tensors['___par_1'] * self.all_tensors['___con_2']
+    self._label = self.all_tensors['___con_1']
+tp.assign_process_process = assign_process
 
 
-v=SimpleExample(tp)
+v=tp.train()
 print(v)
