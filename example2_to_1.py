3cb8a6bd3ffca88d0dc02da13da2e397b718b0f7
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 20:41:43 +0900

    simplify

--- example2_to_1.py
+++ example2_to_1.py
@@ -15,9 +15,12 @@ def SimpleExample(_tp:TorchPlus):
     _tp.all_tensors['___con_1'] = torch.FloatTensor([2.])
     _tp.all_tensors['___con_2'] = torch.FloatTensor([10.])
     _tp.all_tensors['___par_1'] = torch.rand(_tp.all_tensors['___con_1'].shape[-1], requires_grad=True)
-
+    
     for _ in range(_tp.meta_optimizer_epoch):
-        loss = _tp.meta_error_measurement()(_tp.all_tensors['___con_2'],  _tp.all_tensors['___par_1'] * _tp.all_tensors['___con_1'])
+        pred = _tp.all_tensors['___par_1'] * _tp.all_tensors['___con_1']
+        label = _tp.all_tensors['___con_2']
+
+        loss = _tp.meta_error_measurement()(label,  pred)
         optim = _tp.meta_optimizer(_tp.get_all_params(),lr=_tp.meta_optimizer_learning_rate)
         optim.zero_grad()
         loss.backward()
