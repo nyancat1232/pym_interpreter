4152dc72bc9599ff00688307de04fad5e385e5b1
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 21:19:35 +0900

    simplify

--- example1_to_2.py
+++ example1_to_2.py
@@ -17,24 +17,17 @@ tp.meta_optimizer = torch.optim.SGD
 tp.meta_optimizer_epoch = 300
 tp.meta_optimizer_learning_rate = 0.01
 tp.meta_error_measurement = torch.nn.MSELoss
-def SimpleExample(_tp:TorchPlus):
-    #all terminals
-    _tp.all_tensors['___con_1'] = torch.ones(4)
-    _tp.all_tensors['___con_2'] = torch.zeros(3)
-    _tp.all_tensors['___par_1'] = torch.rand(_tp.all_tensors['___con_1'].shape[-1], _tp.all_tensors['___con_2'].shape[0], requires_grad=True)
-    _tp.all_tensors['___par_2'] = torch.rand(_tp.all_tensors['___par_1'].shape[-1], requires_grad=True)
+def assign_values(self):
+    self.all_tensors['___con_1'] = torch.ones(4)
+    self.all_tensors['___con_2'] = torch.zeros(3)
+    self.all_tensors['___par_1'] = torch.rand(self.all_tensors['___con_1'].shape[-1], self.all_tensors['___con_2'].shape[0], requires_grad=True)
+    self.all_tensors['___par_2'] = torch.rand(self.all_tensors['___par_1'].shape[-1], requires_grad=True)
+tp.assign_process_values=assign_values
+def assign_process(self):
+    self._pred = torch.matmul(self.all_tensors['___con_1'], self.all_tensors['___par_1']) + self.all_tensors['___par_2']
+    self._label = self.all_tensors['___con_2']
+tp.assign_process_process = assign_process
 
 
-    
-    for _ in range(_tp.meta_optimizer_epoch):
-        #process
-        pred = torch.matmul(_tp.all_tensors['___con_1'], _tp.all_tensors['___par_1']) + _tp.all_tensors['___par_2']
-        label = _tp.all_tensors['___con_2']
-
-        #train
-        _tp.train_one_step_by_equation(label,pred)
-    return _tp.get_all_params()
-
-
-v=SimpleExample(tp)
+v=tp.train()
 print(v)
