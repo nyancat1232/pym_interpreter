50c1937e2ba83fa1eea9cad27ba00dd371ca22d3
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 16:54:28 +0900

    lval

--- torch_wrap.py
+++ torch_wrap.py
@@ -15,13 +15,13 @@ all_tensors.append(x)
 a = torch.FloatTensor(1)
 a.requires_grad = True
 all_tensors.append(a)
-y = torch.FloatTensor([10])
-all_tensors.append(y)
+reflect_lval = torch.FloatTensor([10])
+all_tensors.append(reflect_lval)
 
 all_params=[l for l in all_tensors if l.requires_grad]
 
 for _ in range(meta_epoch):
-    loss = meta_loss()(y, a*x)
+    loss = meta_loss()(reflect_lval, a*x)
     optim = meta_optimizer(all_params,lr=0.1)
     optim.zero_grad()
     loss.backward()
