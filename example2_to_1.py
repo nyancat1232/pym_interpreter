3204838029bdccbf95f6f118b9d70cb1acdd3700
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 20:42:48 +0900

    comment

--- example2_to_1.py
+++ example2_to_1.py
@@ -12,14 +12,17 @@ tp.meta_optimizer_epoch = 300
 tp.meta_optimizer_learning_rate = 0.01
 tp.meta_error_measurement = torch.nn.MSELoss
 def SimpleExample(_tp:TorchPlus):
+    #all terminals
     _tp.all_tensors['___con_1'] = torch.FloatTensor([2.])
     _tp.all_tensors['___con_2'] = torch.FloatTensor([10.])
     _tp.all_tensors['___par_1'] = torch.rand(_tp.all_tensors['___con_1'].shape[-1], requires_grad=True)
     
     for _ in range(_tp.meta_optimizer_epoch):
+        #process
         pred = _tp.all_tensors['___par_1'] * _tp.all_tensors['___con_1']
         label = _tp.all_tensors['___con_2']
 
+        #train
         loss = _tp.meta_error_measurement()(label,  pred)
         optim = _tp.meta_optimizer(_tp.get_all_params(),lr=_tp.meta_optimizer_learning_rate)
         optim.zero_grad()
