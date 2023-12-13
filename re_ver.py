d020031a9f38ac4b27292b26039b7d8b800549a7
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-13 18:39:33 +0900

    re

--- re_ver.py
+++ re_ver.py
@@ -12,22 +12,4 @@ from pyplus.perceptron.graph import Node,default_optimizer\n'''
     model_block_finder=re.compile(find)
     res_str = model_block_finder.sub(r'',res_str)
 
-    #pop meta props
-    #find=r'\<(.*)\>'
-    #model_block_finder=re.compile(find)
-    #meta_scope=model_block_finder.findall(res_str)
-    #meta_scope=[{v.split('=')[0]:v.split('=')[1] for v in m.split(',')} for m in meta_scope]
-    #res_str = model_block_finder.sub(r'',res_str)
-    #print(meta_scope)
-
-    #class or function wrapper
-    #find=r'((\w+\.)+.\w+\(.*\))'
-    #model_block_finder=re.compile(find)
-    #res_str = model_block_finder.sub(r'Node(\1)',res_str)
-
-    #question mark(func) to parameter
-    find=r'\?(\w+\(\w+\))'
-    model_block_finder=re.compile(find)
-    res_str = model_block_finder.sub(r'Node(\1,is_parameter=True)',res_str)
-
     return pre_str+res_str
--- torch_wrap.py
+++ torch_wrap.py
@@ -4,28 +4,28 @@
 #compile result
 
 import torch
+from pyplus.pytorch.simple import TorchPlus
 
-def SimpleExample():
-    meta_loss = torch.nn.MSELoss
-    meta_optimizer = torch.optim.SGD
-    meta_epoch = 10
+tp = TorchPlus()
+tp.meta_loss = torch.nn.MSELoss
+tp.meta_optimizer = torch.optim.SGD
+tp.meta_epoch = 10
+def SimpleExample(_tp:TorchPlus):
+    _tp.all_tensors['x'] = torch.FloatTensor([2])
+    _tp.all_tensors['a'] = torch.FloatTensor(1)
+    _tp.all_tensors['a'].requires_grad = True
+    _tp.all_tensors['_lval'] = torch.FloatTensor([10])
 
-    all_tensors={}
-    all_tensors['x'] = torch.FloatTensor([2])
-    all_tensors['a'] = torch.FloatTensor(1)
-    all_tensors['a'].requires_grad = True
-    all_tensors['_lval'] = torch.FloatTensor([10])
+    all_params=[_tp.all_tensors[key] for key in _tp.all_tensors if _tp.all_tensors[key].requires_grad]
 
-    all_params=[all_tensors[key] for key in all_tensors if all_tensors[key].requires_grad]
-
-    for _ in range(meta_epoch):
-        loss = meta_loss()(all_tensors['_lval'], all_tensors['a']*all_tensors['x'])
-        optim = meta_optimizer(all_params,lr=0.1)
+    for _ in range(_tp.meta_epoch):
+        loss = _tp.meta_loss()(_tp.all_tensors['_lval'], _tp.all_tensors['a']*_tp.all_tensors['x'])
+        optim = _tp.meta_optimizer(all_params,lr=0.1)
         optim.zero_grad()
         loss.backward()
         optim.step()
     return all_params
 
 
-v=SimpleExample()
+v=SimpleExample(tp)
 print(v)
