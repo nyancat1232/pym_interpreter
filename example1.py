afdbc3bb498902a4585a863b3653747cd0cff2a5
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-14 18:41:13 +0900

    rename

--- example1.py
+++ example1.py
@@ -13,7 +13,7 @@ def assign_values(self:TorchPlus):
     self.all_leaf_tensors[1].requires_grad=True
     self.all_leaf_tensors[2]  = torch.FloatTensor([10])
     self.all_leaf_tensors[2].requires_grad=False
-tp.assign_process_values=assign_values
+tp.assign_leaf_tensors=assign_values
 def assign_process(self:TorchPlus):
     self._pred = self.all_leaf_tensors[1] * self.all_leaf_tensors[0]
     self._label = self.all_leaf_tensors[2]
