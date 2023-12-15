c600bb41066b78eef468cc5dee153831607d3308
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-15 16:03:42 +0900

    more clearly

--- example1.py
+++ example1.py
@@ -1,5 +1,5 @@
 #model SimpleExample:
-#    21 ?= ? * $2
+#    20 ?= ? * $2
 
 import torch
 
@@ -7,24 +7,29 @@ from pyplus.pytorch.simple import TorchPlus,TTPType,TorchTensorPlus
 
 tp = TorchPlus()
 def assign_values(self:TorchPlus):
-    #elf.all_leaf_tensors[0]  = TorchTensorPlus(ttype=TTPType.INPUT)
-    #self.all_leaf_tensors[0].tensor = torch.FloatTensor([2])
-    self.all_leaf_tensors[0]  = TorchTensorPlus(ttype=TTPType.INPUT)
-    self.all_leaf_tensors[0].tensor = torch.FloatTensor([2])
+    #elf.all_leaf_tensors['input0']  = TorchTensorPlus(ttype=TTPType.INPUT)
+    #self.all_leaf_tensors['input0'].tensor = torch.FloatTensor([2])
+    self.all_leaf_tensors['input0']  = TorchTensorPlus(ttype=TTPType.INPUT)
+    self.all_leaf_tensors['input0'].tensor = torch.FloatTensor([2])
     self.all_leaf_tensors[1]  = TorchTensorPlus(ttype=TTPType.PARAMETER)
     self.all_leaf_tensors[1].tensor = torch.FloatTensor(1)
     self.all_leaf_tensors[2]  = TorchTensorPlus(ttype=TTPType.DEFAULT)
-    self.all_leaf_tensors[2].tensor = torch.FloatTensor([21])
+    self.all_leaf_tensors[2].tensor = torch.FloatTensor([20])
 tp.assign_leaf_tensors=assign_values
 def assign_process(self:TorchPlus):
     #.......
     #
     #self._pred = self.all_leaf_tensors[1] * self.all_leaf_tensors[0]
     #self._label = self.all_leaf_tensors[2]
-    self._pred = self.all_leaf_tensors[1].tensor * self.all_leaf_tensors[0].tensor
+    self._pred = self.all_leaf_tensors[1].tensor * self.all_leaf_tensors['input0'].tensor
     self._label = self.all_leaf_tensors[2].tensor
 tp.assign_process_process = assign_process
 
 
-v=tp.train()
-print(v)
+
+print(tp.train())
+print(tp.predict(**{'input0':torch.FloatTensor([12])}))
+print(tp.predict(**{'input0':torch.FloatTensor([10])}))
+print(tp.predict(**{'input0':torch.FloatTensor([13])}))
+print(tp.predict(**{'input0':torch.FloatTensor([14])}))
+print(tp.predict(**{'input0':torch.FloatTensor([12])}))
