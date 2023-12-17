f4f36dc5dfa00bd6f501e300f7295665e6276972
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-18 03:35:54 +0900

    working example

--- example11.py
+++ example11.py
@@ -8,25 +8,25 @@ from pyplus.pytorch.simple import TorchPlus,TTPType,TorchTensorPlus
 
 tp = TorchPlus()
 
+
 #assign leaf tensors
 #tp['input']  = TorchTensorPlus(ttype=TTPType.INPUT,axis_sequence=0)
-#tp['input'].tensor = torch.FloatTensor([[2.],[4.]])
+#tp['input'].tensor = torch.FloatTensor([2.,4.])
+#tp.label_tensor = TorchTensorPlus(ttype=TTPType.DEFAULT,axis_sequence=0)
+#tp.label_tensor.tensor = torch.FloatTensor([18.,36.])
 tp['input']  = TorchTensorPlus(ttype=TTPType.INPUT,axis_sequence=0)
 tp['input'].tensor = torch.FloatTensor([2,4,6,8])
 tp['param']  = TorchTensorPlus(ttype=TTPType.PARAMETER,axis_sequence=-1)
 tp['param'].tensor = torch.FloatTensor(1)
-tp['output']  = TorchTensorPlus(ttype=TTPType.DEFAULT,axis_sequence=0)
-tp['output'].tensor = torch.FloatTensor([10,20,30,40])
+tp.label_tensor = TorchTensorPlus(ttype=TTPType.DEFAULT,axis_sequence=0)
+tp.label_tensor.tensor = torch.FloatTensor([10,20,30,40])
 
 def assign_process(tensors_current_sequence,current_activator):
-    #proc = tensors_current_sequence['input'] @ tensors_current_sequence['param']
     proc = tensors_current_sequence['param']*tensors_current_sequence['input']
 
-    _pred = proc
-    _label = tensors_current_sequence['output']
+    return proc
+tp.assign_process_prediction = assign_process
 
-    return _label,_pred
-tp.assign_process_process = assign_process
 
 print(tp.train())
 print(tp.predict(**{'input':torch.FloatTensor([1,2,3,4])}))
