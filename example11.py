30e037f974601ba529e1614270056f04aa59a6ed
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-17 19:57:29 +0900

    update

--- example11.py
+++ example11.py
@@ -1,32 +1,38 @@
 #!sequence
 #model SimpleExampleSequence:
-#    [10,20,30,40] ?= ? * $[2,4,6,8]
+#    [10,20,30,40] ?= ?1 * $[2,4,6,8]
 
 import torch
 import torch.nn as nn
 from pyplus.pytorch.simple import TorchPlus,TTPType,TorchTensorPlus
 
 tp = TorchPlus()
-tp.is_sequence=True
+
 tp.meta_activator = nn.ReLU
 tp.meta_optimizer = torch.optim.SGD
 tp.meta_optimizer_epoch = 300
-tp.meta_optimizer_learning_rate = 0.015
+tp.meta_optimizer_params = {'lr':0.015}
 tp.meta_error_measurement = torch.nn.MSELoss
-def assign_values(self:TorchPlus):
-    self.all_leaf_tensors['input']  = TorchTensorPlus(ttype=TTPType.INPUT)
-    self.all_leaf_tensors['input'].tensor = torch.FloatTensor([2,4,6,8])
-    self.all_leaf_tensors['output']  = TorchTensorPlus(ttype=TTPType.DEFAULT)
-    self.all_leaf_tensors['output'].tensor = torch.FloatTensor([10,20,30,40])
-    self.all_leaf_tensors['___0']  = TorchTensorPlus(ttype=TTPType.PARAMETER)
-    self.all_leaf_tensors['___0'].tensor = torch.FloatTensor(1)
-tp.assign_leaf_tensors=assign_values
-def assign_process(self:TorchPlus,current_sequence:int):
-    proc = self.all_leaf_tensors['input'][current_sequence] * self.all_leaf_tensors['___0'].tensor
 
-    self._pred = proc
-    self._label = self.all_leaf_tensors['output'][current_sequence]
+#assign leaf tensors
+#tp['input']  = TorchTensorPlus(ttype=TTPType.INPUT,axis_sequence=0)
+#tp['input'].tensor = torch.FloatTensor([[2.],[4.]])
+tp['input']  = TorchTensorPlus(ttype=TTPType.INPUT,axis_sequence=0)
+tp['input'].tensor = torch.FloatTensor([2,4,6,8])
+tp['param']  = TorchTensorPlus(ttype=TTPType.PARAMETER,axis_sequence=-1)
+tp['param'].tensor = torch.FloatTensor(1)
+tp['output']  = TorchTensorPlus(ttype=TTPType.DEFAULT,axis_sequence=0)
+tp['output'].tensor = torch.FloatTensor([10,20,30,40])
+
+def assign_process(tensors_current_sequence,current_activator):
+    #proc = tensors_current_sequence['input'] @ tensors_current_sequence['param']
+    proc = tensors_current_sequence['param']*tensors_current_sequence['input']
+
+    _pred = proc
+    _label = tensors_current_sequence['output']
+
+    return _label,_pred
 tp.assign_process_process = assign_process
 
 print(tp.train())
-print(tp.predict(**{'input':torch.FloatTensor([11])}))
+print(tp.predict(**{'input':torch.FloatTensor([1,2,3,4])}))
--- example6.py
+++ example6.py
@@ -1,6 +1,6 @@
 #!activator ReLU
 #model Xor:
-#    [[0],[1],[1],[0]] ?= $[[0,0],[0,1],[1,0],[0,0]] @ ? #? automatically converted as 2x1
+#    [[0],[1],[1],[0]] ?= ($[[0,0],[0,1],[1,0],[0,0]]+[[1,1,1,1]]) @ ? #? automatically converted as 2x1
 
 import torch
 import torch.nn as nn
@@ -17,14 +17,16 @@ tp.meta_error_measurement = torch.nn.MSELoss
 #assign leaf tensors
 tp['input']  = TorchTensorPlus(ttype=TTPType.INPUT,axis_sequence=0)
 tp['input'].tensor = torch.FloatTensor([[0,0],[0,1],[1,0],[1,1]])
-tp['param']  = TorchTensorPlus(ttype=TTPType.PARAMETER,axis_sequence=-1)
-tp['param'].tensor = torch.FloatTensor(2,1)
+tp['def1']  = TorchTensorPlus(ttype=TTPType.PARAMETER,axis_sequence=-1)
+tp['def1'].tensor = torch.FloatTensor(2,1)
+tp['param2']  = TorchTensorPlus(ttype=TTPType.PARAMETER,axis_sequence=0)
+tp['param2'].tensor = torch.FloatTensor(2,1)
 tp['output']  = TorchTensorPlus(ttype=TTPType.DEFAULT,axis_sequence=0)
 tp['output'].tensor = torch.FloatTensor([[0],[1],[1],[0]])
 
 def assign_process(tensors_current_sequence,current_activator):
     #proc = tensors_current_sequence['input'] @ tensors_current_sequence['param']
-    proc = tensors_current_sequence['input'] @ tensors_current_sequence['param']
+    proc = tensors_current_sequence['input'] @ tensors_current_sequence['param2']
     proc = current_activator(proc)
     _pred = proc
     _label = tensors_current_sequence['output']
