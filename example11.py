451409a3cb5b8e8ae6322ba2860b75aeb1d2e91f
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-20 18:40:50 +0900

    add

--- example11.py
+++ example11.py
@@ -16,5 +16,5 @@ def assign_process(current_activator):
 tp.process = assign_process
 
 
-result = tp.train(show_epoch_process=True)
+result = tp.train(show_every_iteration=True)
 print(result(input=torch.FloatTensor([1,2,3,4,1,2,3])))
