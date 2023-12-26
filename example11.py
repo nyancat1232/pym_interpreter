2948e345f15e4f5a1e90952d8686f7c51e726469
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-26 10:44:51 +0900

    add name of keyword

--- example11.py
+++ example11.py
@@ -8,7 +8,7 @@ from simpletorch.simple import TorchPlus,MetaDataType
 
 class Test(TorchPlus):
     def process(self):
-        proc = self.parameter([1],'param')*self.input([2,4,6,8],'input',MetaDataType.NUMERICAL)
+        proc = self.parameter([1],'param')*self.input([2,4,6,8],meta_data_type=MetaDataType.NUMERICAL,name='input')
 
         self.label([10,20,30,40],MetaDataType.NUMERICAL)
         return proc
