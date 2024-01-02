e0d66a53704dfc17025d2484b6df1e1350ee6290
Lua Jeon <nyancat2323.1232@gmail.com>
2024-01-02 10:48:19 +0900

    add testcase

--- test_st.py
+++ test_st.py
@@ -4,6 +4,9 @@ import numpy as np
 
 from simpletorch.simple import TorchPlus,MetaDataType,CurrentStateInformation
 
+def type_to(tens:torch.Tensor):
+    return tens.detach().numpy()[0]
+
 class SimpleTorchChecker(TestCase):
     def test_example1(self):
         class Test(TorchPlus):
@@ -25,4 +28,29 @@ class SimpleTorchChecker(TestCase):
         result_result = result(input=torch.FloatTensor(tests))
 
         for index,label in enumerate(tests_label):
-            self.assertAlmostEqual(result_result[index].detach().numpy()[0], label,delta=1e-1)
+            self.assertAlmostEqual(type_to(result_result[index]), label,delta=1e-1)
+
+    def test_example5(self):
+        x=[[73, 80, 75],
+            [93, 88, 93],
+            [89, 91, 90],
+            [96, 98, 100],
+            [73, 66, 70]]
+        label = [152, 185, 180, 196, 142]
+
+        class Test(TorchPlus):
+            def process(self):
+                proc = self.input('input',x,MetaDataType.NUMERICAL) @ self.parameter('param',(3,1))
+                proc = proc.squeeze()
+                self.label(label,MetaDataType.NUMERICAL)
+                return proc
+            def show_progress(self,csi:CurrentStateInformation):
+                print(f'Epoch : {csi.current_epoch} \tIteration : {csi.current_iteration}/{csi.len_iteration}\tLoss : {csi.current_loss}')
+
+
+        tp = Test(meta_optimizer = torch.optim.SGD,meta_optimizer_params = {'lr':1e-5},
+                meta_data_per_iteration = 3)
+
+        result = tp.train(epoch=10000)
+        for x,label in zip(x,label):
+            self.assertAlmostEqual(result(input=torch.FloatTensor([x]))[0].detach().numpy(),label,delta=1e+1)
